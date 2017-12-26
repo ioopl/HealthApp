8237bb406e31972e41feeac56c11fcb6cab25527
Umair Hasan <umair.h@live.co.uk>
2017-12-26 14:42:37 +0000

    Updated Appointment Response Properties according to JSON.

--- HealthApp/Model/MessageResponse.swift
+++ HealthApp/Model/MessageResponse.swift
@@ -26,14 +26,14 @@ struct MessageResponse {
 struct AppointmentResponse {
     // MARK: Variables
     var id = String()
-    var sender_name = String()
-    var body = String()
+    var doctor_name = String()
+    var start_at = String()
     
     init(dictionary : [[String:Any]]) {
         for result in dictionary {
             id = result["id"] as! String
-            sender_name = result["sender_name"] as! String
-            body = result["body"] as! String
+            doctor_name = result["doctor_name"] as! String
+            start_at = result["start_at"] as! String
         }
     }
 }
